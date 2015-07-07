41088044ec52dd4f3d1a6a5bcd125f6f60c687ad
UniPixen <UniPixen@users.noreply.github.com>
2015-07-07 20:31:55 +0530

    v1

---
+++ oauth.php
@@ -0,0 +1,11 @@
+<?php
+
+//Oauth script with oauth id
+
+
+
+$GLOBALS['oauth_id']		=	"envmetricslivetest-zk9o11x7";
+$GLOBALS['redirect_uri']	=	"http://envmetrics.com/member"; 
+$GLOBALS['secret']              =       "nLhBcDWXeZ4CiczNnX8C7HA1m9lexEm2";
+
+?>
