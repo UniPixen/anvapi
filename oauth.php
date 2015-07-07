50e9eb6d2ba7eb7d83a33a9150fbc0cebca206bc
UniPixen <UniPixen@users.noreply.github.com>
2015-07-07 20:34:52 +0530

    v1.0

---
+++ oauth.php
@@ -0,0 +1,11 @@
+<?php
+
+//Oauth script with oauth id
+
+
+
+$GLOBALS['oauth_id']		=	"REPLACE THIS WITH oauth ID";
+$GLOBALS['redirect_uri']	=	"http://envmetrics.com/member"; 
+$GLOBALS['secret']              =       "REPLACE THIS WITH SECRET KEY";
+
+?>
