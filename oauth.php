44f8a3fa8673681f99a7fcfe55f21870c80df425
UniPixen <UniPixen@users.noreply.github.com>
2015-07-07 20:32:20 +0530

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -6,6 +6,6 @@
 
 $GLOBALS['oauth_id']		=	"envmetricslivetest-zk9o11x7";
 $GLOBALS['redirect_uri']	=	"http://envmetrics.com/member"; 
-$GLOBALS['secret']              =       "nLhBcDWXeZ4CiczNnX8C7HA1m9lexEm2";
+$GLOBALS['secret']              =       "REPLACE WITH SECRET KEY";
 
 ?>
